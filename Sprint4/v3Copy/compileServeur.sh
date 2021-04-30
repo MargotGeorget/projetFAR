6a90ae3525c4280e0434a082f2a5256a09bf1e1b
margot.georget <margot.georget@etu.umontpellier.fr>
2021-04-30 14:23:13 +0200

    update compileServeur

--- Sprint4/v3Copy/compileServeur.sh
+++ Sprint4/v3Copy/compileServeur.sh
@@ -1,1 +1,1 @@
-gcc -pthread -Wall -ansi -o serveur serveur.c FunctionsServeur/dialogFunctions.c FunctionsServeur/managementClient.c FunctionsServeur/managementMessage.c FunctionsServeur/threadFunctions.c
+gcc -pthread -Wall -ansi serveur.c FunctionsServeur/dialogFunctions.c FunctionsServeur/managementClient.c FunctionsServeur/managementMessage.c FunctionsServeur/threadFunctions.c -o serveur
